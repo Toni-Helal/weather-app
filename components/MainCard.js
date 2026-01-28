eef8aad63db31aaac750d1154396915e6a4a2d31
Antoun Helal <antounhelal@MacBook-Pro-de-Antoun.local>
2026-01-29 00:15:10 +0100

    Simplify MainCard to support Open-Meteo data

--- components/MainCard.js
+++ components/MainCard.js
@@ -1,40 +1,16 @@
-import Image from "next/image";
-import { ctoF } from "../services/converters";
 import styles from "./MainCard.module.css";
 
-export const MainCard = ({
-  city,
-  country,
-  description,
-  iconName,
-  unitSystem,
-  weatherData,
-}) => {
+export function MainCard({ temperature, windSpeed, time }) {
+  const date = new Date(time);
+
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.location}>
-        {city}, {country}
-      </h1>
-      <p className={styles.description}>{description}</p>
-      <Image
-        width="300px"
-        height="300px"
-        src={`/icons/${iconName}.svg`}
-        alt="weatherIcon"
-      />
+    <div className={styles.card}>
       <h1 className={styles.temperature}>
-        {unitSystem == "metric"
-          ? Math.round(weatherData.main.temp)
-          : Math.round(ctoF(weatherData.main.temp))}
-        °{unitSystem == "metric" ? "C" : "F"}
+        {Math.round(temperature)}°C
       </h1>
-      <p>
-        Feels like{" "}
-        {unitSystem == "metric"
-          ? Math.round(weatherData.main.feels_like)
-          : Math.round(ctoF(weatherData.main.feels_like))}
-        °{unitSystem == "metric" ? "C" : "F"}
-      </p>
+
+      <p>Wind: {Math.round(windSpeed)} km/h</p>
+      <p>{date.toLocaleString()}</p>
     </div>
   );
-};
+}
