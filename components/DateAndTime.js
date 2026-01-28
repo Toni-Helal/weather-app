71d695772a8b99eecd9146cf4b74eda0cee4d7e1
Antoun Helal <antounhelal@MacBook-Pro-de-Antoun.local>
2026-01-29 00:42:21 +0100

    Remove MetricsBox incompatible with Open-Meteo API

--- components/DateAndTime.js
+++ components/DateAndTime.js
@@ -1,16 +1,8 @@
 import { getWeekDay, getTime, getAMPM } from "../services/helpers";
 import styles from "./DateAndTime.module.css";
 
-export const DateAndTime = ({ weatherData, unitSystem }) => {
-  return (
-    <div className={styles.wrapper}>
-      <h2>
-        {`${getWeekDay(weatherData)}, ${getTime(
-          unitSystem,
-          weatherData.dt,
-          weatherData.timezone
-        )} ${getAMPM(unitSystem, weatherData.dt, weatherData.timezone)}`}
-      </h2>
-    </div>
-  );
-};
+export function DateAndTime({ time }) {
+  const date = new Date(time);
+
+  return <p>{date.toLocaleString()}</p>;
+}
